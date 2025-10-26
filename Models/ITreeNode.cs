70ebec9f326370d0097a0a28568af9efd5852132
Steve <stephen.r.strong@saic.com>
2025-10-26 15:53:03 -0400

    feat: reorganize ITreeNode interface and move TreeNodeAction and TreeNodeReference records to the top

--- Models/ITreeNode.cs
+++ Models/ITreeNode.cs
@@ -1,18 +1,22 @@
 using FoundryRulesAndUnits.Extensions;
 namespace FoundryRulesAndUnits.Models;
 
+public record TreeNodeAction(string Name, string Style, Action Action);
+
+public record TreeNodeReference(StatusBitArray Status, ITreeNode Node);
+
 public interface ITreeNode
 {
     public string GetUniqueId(); // Stable unique identifier for caching
-    
+
     public bool GetIsExpanded();
     public bool SetExpanded(bool value);
 
     public bool GetIsSelected();
     public bool SetSelected(bool value);
 
-    public string GetTreeNodeTitle(); 
-    public IEnumerable<TreeNodeAction>? GetTreeNodeActions(); 
+    public string GetTreeNodeTitle();
+    public IEnumerable<TreeNodeAction>? GetTreeNodeActions();
     public IEnumerable<ITreeNode> GetTreeChildren();
 
     public bool HasChildren() => GetTreeChildren()?.Any() ?? false;
@@ -27,31 +31,30 @@ public interface ITreeNode
             child.PrintToConsole(indent + 1);
     }
 }
-public record TreeNodeAction(string Name, string Style, Action Action);
 
-public static class TreeNodeExtensions
-{
-    public static IEnumerable<ITreeNode> GetAllDescendants(this ITreeNode node)
-    {
-        var stack = new Stack<ITreeNode>();
-        stack.Push(node);
-        while (stack.Any())
-        {
-            var current = stack.Pop();
-            yield return current;
-            foreach (var child in current.GetTreeChildren())
-                stack.Push(child);
-        }
-    }
+// public static class TreeNodeExtensions
+// {
+//     public static IEnumerable<ITreeNode> GetAllDescendants(this ITreeNode node)
+//     {
+//         var stack = new Stack<ITreeNode>();
+//         stack.Push(node);
+//         while (stack.Any())
+//         {
+//             var current = stack.Pop();
+//             yield return current;
+//             foreach (var child in current.GetTreeChildren())
+//                 stack.Push(child);
+//         }
+//     }
 
-    public static TreeNodeAction AddAction(this List<TreeNodeAction> list, string name, string style, Action action)
-    {
-        var nodeaction = new TreeNodeAction(name, style, action);
-        list.Add(nodeaction);
-        return nodeaction;
-    }
+//     public static TreeNodeAction AddAction(this List<TreeNodeAction> list, string name, string style, Action action)
+//     {
+//         var nodeaction = new TreeNodeAction(name, style, action);
+//         list.Add(nodeaction);
+//         return nodeaction;
+//     }
 
-}
+// }
 
 
 
