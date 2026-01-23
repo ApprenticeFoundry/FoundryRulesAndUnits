469e077578298caedcbd47f7e8a11db7bc713950
Steve <stephen.r.strong@saic.com>
2026-01-22 22:53:55 -0500

    refactor: Remove ConsoleHelpers class and update DT_Hero to use FoundryMicroCore.Core.Extensions

--- DataModels/DT_Hero.cs
+++ DataModels/DT_Hero.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using FoundryRulesAndUnits.Extensions;
+using FoundryMicroCore.Core.Extensions;
 
 namespace FoundryRulesAndUnits.Models;
 
